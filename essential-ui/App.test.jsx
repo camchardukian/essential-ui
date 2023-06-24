a4edefda6752d3f56084576cda635077284abf76
Cameron Chardukian <camchardukian@gmail.com>
2023-06-24 20:33:48 +0700

    feature/github-actions -- tried to add hello action

---
+++ essential-ui/App.test.jsx
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+describe('something truthy and falsy', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should be false', () => {
+    expect(false).toBe("this test should fail");
+  });
+});
